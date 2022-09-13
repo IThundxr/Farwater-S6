86ca48d9d8816bbbfcb92d5e3cd37f4f4e86a045
Shuro <944030+Shuro@users.noreply.github.com>
2022-09-13 23:14:27 +0200

    server_scripts: Allow Lantern to hang on create-brackets

---
+++ kubejs/server_scripts/create.js
@@ -0,0 +1,25 @@
+priority: 1
+
+settings.logAddedRecipes = true
+settings.logRemovedRecipes = true
+settings.logSkippedRecipes = false
+settings.logErroringRecipes = true
+
+var seed
+var log = []
+
+onEvent('block.right_click', event => {
+    if (event.item.hasTag('c:lanterns')) {
+        let bracket = event.block.entityData.Bracket.Properties
+        if (bracket) {
+            if (bracket.facing == 'up' && event.block.down.id == 'minecraft:air') {
+                event.block.down.set(event.item.id, {hanging: true})
+                event.player.playSound('block.lantern.place')
+            } else if (bracket.facing == 'down' && event.block.up.id == 'minecraft:air') {
+                event.block.up.set(event.item.id, {hanging: false})
+                event.player.playSound('block.lantern.place')
+            }
+            if (!event.player.creativeMode) { event.player.getHeldItem(event.hand).count -= 1 }
+        }
+    }
+});
--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -1,10 +1,13 @@
 priority: 0
 
 onEvent('tags.items', event => {
-    //adds the create dough to the doughs collection tag
+    //adds the doughs to the doughs collection tag
     event.add('forge:doughs', 'create:dough')
-    //adds the create dough to the doughs collection tag
     event.add('forge:doughs', 'farmersdelight:wheat_dough')
-  //adds the create dough to the doughs collection tag
     event.add('forge:doughs', 'croptopia:dough')
+    
+    //adds the lanterns to the lantern tag
+    event.add('c:lanterns', 'minecraft:lantern')
+    event.add('c:lanterns', 'minecraft:soul_lantern')
+    event.add('c:lanterns', 'architects_palette:nether_brass_lantern')
   })
