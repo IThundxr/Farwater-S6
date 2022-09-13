b629da7666b7b542543964c7b683cb18f2be4fc8
Shuro <944030+Shuro@users.noreply.github.com>
2022-09-13 22:54:57 +0200

    server_scripts: Remove items from loot.js
    Some Items were not longer available, therefore they cause problems with
    loot tables.

--- kubejs/server_scripts/loot.js
+++ kubejs/server_scripts/loot.js
@@ -7,41 +7,17 @@ onEvent('chest.loot_tables', event => {
 			pool.rolls = [2, 8]
 			pool.addItem('create:andesite_alloy', 15)
 		})
-		table.addPool(pool => {
-			pool.rolls = [0, 8]
-			pool.addItem('kubejs:bronze_coin', 15)
-		})
-	})
-	event.modify('minecraft:village/village_butcher', table => {
-		table.addPool(pool => {
-			pool.rolls = [0, 8]
-			pool.addItem('kubejs:bronze_coin', 15)
-		})
-	})
-	event.modify('minecraft:village/village_cartographer', table => {
-		table.addPool(pool => {
-			pool.rolls = [0, 8]
-			pool.addItem('kubejs:bronze_coin', 15)
-		})
 	})
 	event.modify('minecraft:village/village_toolsmith', table => {
 		table.addPool(pool => {
 			pool.rolls = [2, 8]
 			pool.addItem('create:andesite_alloy', 15)
 		})
-		table.addPool(pool => {
-			pool.rolls = [0, 8]
-			pool.addItem('kubejs:bronze_coin', 15)
-		})
 	})
 	event.modify('minecraft:village/village_weaponsmith', table => {
 		table.addPool(pool => {
 			pool.rolls = [2, 8]
 			pool.addItem('create:andesite_alloy', 15)
-		})
-		table.addPool(pool => {
-			pool.rolls = [0, 8]
-			pool.addItem('kubejs:bronze_coin', 15)
 		})
 	})
 })
