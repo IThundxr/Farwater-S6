b61bfc72f4707d68849a800ad6442ee9ed6820a1
Shuro <944030+Shuro@users.noreply.github.com>
2022-09-14 19:06:29 +0200

    client_scripts: convert jei.js to rei.js

--- kubejs/client_scripts/rei.js
+++ kubejs/client_scripts/rei.js
@@ -1,14 +1,10 @@
 // JEI Subtype
-onEvent('jei.subtypes', event => {
+onEvent('rei.subtypes', event => {
 	event.useNBTKey('kubejs:trade_card', 'CustomModelData')
 })
 // JEI Add
-onEvent('jei.add.items', event => {
+onEvent('rei.add.items', event => {
 	event.add(Item.of('create:refined_radiance'))
-	event.add(Item.of('create:refined_radiance_casing'))
-	event.add(Item.of('create:shadow_steel'))
-	event.add(Item.of('create:shadow_steel_casing'))
-	
 	
     event.add(Item.of('kubejs:trade_card', "{CustomModelData:1,display:{Name:'{\"text\":\"Hunter Export Card\",\"italic\":\"false\",\"color\":\"light_purple\"}'}}"))
     event.add(Item.of('kubejs:trade_card', "{CustomModelData:2,display:{Name:'{\"text\":\"Lumber Export Card\",\"italic\":\"false\",\"color\":\"light_purple\"}'}}"))
@@ -18,20 +14,8 @@ onEvent('jei.add.items', event => {
 })
 
 // JEI Hide
-onEvent('jei.hide.items', event => {
+onEvent('rei.hide.items', event => {
 	event.hide('createaddition:copper_connector')
-	
-	//event.hide('computercraft:turtle_normal')
-	//event.hide('computercraft:turtle_advanced')
 
 	event.hide('farwateraddons:ithundxr_item')
-	
-	event.hide('thermal:carrot_block')
-    	event.hide('thermal:potato_block')
-    	event.hide('thermal:beetroot_block')
-    	event.hide('thermal:corn_block')
-    	event.hide('thermal:onion_block')
-    	event.hide('thermal:eggplant_block')
-    	event.hide('thermal:tomato_block')
-
 })
