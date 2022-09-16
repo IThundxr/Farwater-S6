f71615bb9060dad1c21a7e936d4845db6ee0cc6a
Shuro <944030+Shuro@users.noreply.github.com>
2022-09-16 21:01:20 +0200

    create.js:  null-safe property access of brackets

--- kubejs/server_scripts/create.js
+++ kubejs/server_scripts/create.js
@@ -10,7 +10,7 @@ var log = []
 
 onEvent('block.right_click', event => {
     if (event.item.hasTag('c:lanterns')) {
-        let bracket = event.block.entityData.Bracket.Properties
+        let bracket = event.block.entityData?.Bracket?.Properties
         if (bracket) {
             if (bracket.facing == 'up' && event.block.down.id == 'minecraft:air') {
                 event.block.down.set(event.item.id, {hanging: true})
