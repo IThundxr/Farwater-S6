2d79490a6a6c97bb6c66a61296b8c5ae43ebed62
Shuro <944030+Shuro@users.noreply.github.com>
2022-09-16 20:59:44 +0200

    server_scripts: Slow Fall at Spawn Area

---
+++ kubejs/server_scripts/spawn.js
@@ -0,0 +1,19 @@
+priority: 1
+
+settings.logAddedRecipes = true
+settings.logRemovedRecipes = true
+settings.logSkippedRecipes = false
+settings.logErroringRecipes = true
+
+var seed
+var log = []
+
+onEvent('player.tick', event => {
+    const { x, y, z } = event.player
+    if (x < -2120 && x > -2170 &&
+        y < 131 && y > 128 &&
+        z < 1390 && z > 1330
+    ) {
+        event.player.potionEffects.add('slow_falling')
+    }
+})
