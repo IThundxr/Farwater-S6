a7707a180fcbc57a23965dc1bd61a459540a9a83
IThundxr <harshdhaliwal9767@gmail.com>
2022-09-18 13:28:43 -0400

    Update recipes.js

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -166,7 +166,6 @@ onEvent('recipes', event => {
 
 
 	// Abuseable
-	event.remove({ output: 'tconstruct:efln_ball' })
 	event.remove({ output: 'tconstruct:ender_slime_sling' })
 
 	// To make trade station work
