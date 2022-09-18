a629b1f8e2a87c9f1911db1d7e50343b2d69de0e
Shuro <944030+Shuro@users.noreply.github.com>
2022-09-18 12:25:50 +0200

    fixed coordinates

--- kubejs/server_scripts/spawn.js
+++ kubejs/server_scripts/spawn.js
@@ -10,7 +10,7 @@ var log = []
 
 onEvent('player.tick', event => {
     const { x, y, z } = event.player
-    if (x < -2130 && x > -2160 &&
+    if (x < -2170 && x > -2120 &&
         y < 132 && y > 120 &&
         z < 1400 && z > 1320
     ) {
